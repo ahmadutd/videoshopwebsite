7f8d3182ca6a04fac2ac768a677a735b3ac57f9c
Ahmad <ahmadbaghlanii@gmail.com>
2020-11-26 23:23:31 +0330

    add context class for communication with database by EF core

---
+++ VideoShop.Data/VideoShopDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoShop.Data
+{
+    // this class created for communication with database by EF core.
+    public class VideoShopDbContext
+    {
+        public VideoShopDbContext()
+        {
+
+        }
+    }
+}
