b43a40119336ad0aa0ec6496f4d7aa14d158e377
Ahmad <ahmadbaghlanii@gmail.com>
2020-12-05 21:52:40 +0330

    initial commit

--- VideoShop.Web/Startup.cs
+++ VideoShop.Web/Startup.cs
@@ -50,7 +50,7 @@ namespace VideoShop.Web
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Site}/{action=Index}/{id?}");
             });
         }
     }
